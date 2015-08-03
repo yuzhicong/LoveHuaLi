42f2c7b0d1f1dcaa153cd794cd83e15db6b32538
Zhangsongsong <2964287498@qq.com>
2015-08-03 19:28:59 +0800

    注释

--- app/src/main/java/com/yzc/lovehuali/EditCourseActivity.java
+++ app/src/main/java/com/yzc/lovehuali/EditCourseActivity.java
@@ -5,15 +5,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
@@ -30,8 +27,7 @@ public class EditCourseActivity extends ActionBarActivity {
     private Toolbar mToolbar;
 
     private ACache aCache;
-
-
+//注释
     private int temp = 0;
     private int temp_save = 0;
 
